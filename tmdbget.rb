3ae7634466d98720179db5d382e7c42065dc79fc
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2018-05-17 14:36:53 -0400

    Add "append" functionality - append a selection of movie or tv subfields to the results e.g. keywords, recommendations, videos, etc.

--- tmdbget.rb
+++ tmdbget.rb
@@ -15,11 +15,12 @@ def argparse
   # Defaults
   args = Hash.new(nil)
   args = {
-    maxshow: 4,
-    year: '',
+    'append' => '',
+    'maxshow' => 4,
+    'year' => '',
   }
   required = [
-    'key'
+    'key',
   ]
 
   # rubocop:disable Metrics/BlockLength:
@@ -36,43 +37,51 @@ def argparse
 
     opts.on('-y YEAR', '--year YEAR',
             'Year of release. This may do nothing for TV search.') do |year|
-      args[:year] = year
+      args['year'] = year
     end
 
     opts.on('--yaml',
             'Output in YAML for easier human-reading.') do |yaml|
-      args[:yaml] = yaml
+      args['yaml'] = yaml
+    end
+
+    opts.on('-a x,y,z', '--append x,y,z',
+            'Additional requests from within the same namespace (like credits, images, '\
+            "recommendations) to deliver along with the results. \n"\
+            'See [https://developers.themoviedb.org/3/tv] and '\
+            '[https://developers.themoviedb.org/3/movies for a detailed list of options, or just '\
+            'specify "all".') do |append|
+      args['append'] = append
     end
 
     opts.on('-k KEY', '--key KEY',
             'TMDB API key or the path to a file containing such a key.') do |key|
-      keyval = if File.file?(key)
-        File.open(key) { |f| f.readline.chomp }
-      else
-        key
-      end
-      args[:key] = keyval
+      args['key'] = if File.file?(key)
+                      File.open(key) { |f| f.readline.chomp }
+                    else
+                      key
+                    end
     end
 
     opts.on('-i', '--interactive',
             'Enable in order to present search results for selection on STDERR, eventually '\
             'printing the final selection to STDOUT.') do |interactive|
-      args[:interactive] = interactive
+      args['interactive'] = interactive
     end
 
     opts.on('-m MAXSHOW', '--maxshow MAXSHOW',
             'Limit search results. By default, returns all.') do |maxshow|
-      args[:maxshow] = maxshow.to_i
+      args['maxshow'] = maxshow.to_i
     end
 
     opts.on('--nopretty',
             'Disable pretty-printing of JSON output.') do |nopretty|
-      args[:nopretty] = nopretty
+      args['nopretty'] = nopretty
     end
 
     opts.on('-t', '--tv',
             'Search TV instead of Movies.') do |tv|
-      args[:tv] = tv
+      args['tv'] = tv
     end
   end
   # rubocop:enable Metrics/BlockLength:
@@ -80,16 +89,16 @@ def argparse
 
   # Positional arguments
   if ARGV.size != 1
-    puts 'Missing positional arguments. Run with -h for usage.'
+    STDERR.puts 'Missing positional arguments. Run with -h for usage.'
     exit 1
   else
-    args[:title] = ARGV[0]
+    args['title'] = ARGV[0]
   end
 
   # Required options
   required.each do |optname|
-    unless args[optname.to_sym]
-      puts "Did not provide required argument '--#{optname}'. Run with -h for usage."
+    unless args[optname]
+      STDERR.puts "Did not provide required argument '--#{optname}'. Run with -h for usage."
       exit 1
     end
   end
@@ -98,8 +107,26 @@ def argparse
 end
 # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
-def main
+def query_api(uri)
   require 'faraday'
+  require 'json'
+
+  begin
+    response = Faraday.get(uri)
+    jsondata = JSON.parse(response.body)
+    if jsondata['status_code']
+      STDERR.puts "THE API says: #{jsondata['status_message']}"
+      exit 1
+    end
+  rescue Faraday::Error
+    STDERR.puts 'Oh noes, API GET failed..'
+    raise
+  end
+
+  return jsondata
+end
+
+def main
   require 'json'
   require 'cgi'
 
@@ -108,16 +135,53 @@ def main
     exit 130
   end
 
+  subresources = {
+    'tv' => [
+			'account_states',
+			'alternative_titles',
+			'changes',
+			'content_ratings',
+			'credits',
+			'external_ids',
+			'images',
+			'keywords',
+			'lists',
+			'recommendations',
+			'release_dates',
+			'reviews',
+			'screened_theatrically',
+			'similar',
+			'translations',
+			'videos',
+    ],
+		'movie' => [
+			'account_states',
+			'alternative_titles',
+			'changes',
+			'credits',
+			'external_ids',
+			'images',
+			'keywords',
+			'release_dates',
+			'videos',
+			'translations',
+			'recommendations',
+			'similar',
+			'reviews',
+			'lists',
+		],
+  }
+
   args = argparse
 
-  apikey = args[:key]
-  interactive = args[:interactive]
-  max_show = args[:maxshow]
-  nopretty = args[:nopretty]
-  searchtv = args[:tv]
-  title = args[:title]
-  yaml = args[:yaml]
-  year = args[:year]
+  apikey = args['key']
+  interactive = args['interactive']
+  max_show = args['maxshow']
+  nopretty = args['nopretty']
+  searchtv = args['tv']
+  title = args['title']
+  yaml = args['yaml']
+  year = args['year']
 
   if !searchtv
     querytype = 'movie'
@@ -128,29 +192,23 @@ def main
     date_field = 'first_air_date'
     title_field = 'name'
   end
+  append_resources = if args['append'] == 'all'
+                       subresources[querytype].join(',')
+                     else
+                       args['append']
+                     end
+
   uri = 'https://api.themoviedb.org/3/search/'\
         "#{querytype}?"\
         "api_key=#{apikey}"\
         "&query=#{CGI.escape(title)}"\
         "&year=#{CGI.escape(year)}"
-  begin
-    response = Faraday.get(uri)
-    jsondata = JSON.parse(response.body)
-    if jsondata['status_code']
-      STDERR.puts results['status_message']
-      raise Faraday::Error
-    end
-    results = jsondata['results'][0..max_show - 1]
-  rescue Faraday::Error
-    STDERR.puts 'Oh noes, API GET failed..'
-    raise
-  end
 
+  results = query_api(uri)['results'][0..max_show - 1]
   if results.empty?
     STDERR.puts 'Zero results returned.'
     exit 3
   end
-
   if interactive
     STDERR.puts 'Interactive mode selected. Showing top results...'
     STDERR.puts
@@ -162,10 +220,16 @@ def main
     STDERR.puts
     STDERR.print 'Select a title by [index]: '
     sel_idx = STDIN.gets.chomp.to_i - 1
-    selection = results[sel_idx]
+    selection_id = results[sel_idx]['id']
   else
-    selection = results[0]
+    selection_id = results[0]['id']
   end
+
+  # Get the final selection plus all specified subresources
+  uri = "https://api.themoviedb.org/3/#{querytype}/#{selection_id}?"\
+        "api_key=#{apikey}"\
+        "&append_to_response=#{CGI.escape(append_resources)}"
+  selection = query_api(uri)
 
   # Altering the final selection data structure
   # Add a new year-only date field
